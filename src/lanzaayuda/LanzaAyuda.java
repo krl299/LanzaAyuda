389c19ef1c388cb051f75d6f0ce2c13f3a489f9d
krl299 <47522008+krl299@users.noreply.github.com>
2020-06-04 00:06:48 +0200

    LanzaAyuda terminado.

---
+++ src/lanzaayuda/LanzaAyuda.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lanzaayuda;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.net.URL;
+import javax.help.HelpBroker;
+import javax.help.HelpSet;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+
+/**
+ *
+ * @author usuario
+ */
+public class LanzaAyuda implements ActionListener, ItemListener {
+
+    JTextArea output;
+    JScrollPane scrollPane;
+    String newline = "\n";
+
+    public JMenuBar createMenuBar() {
+        JMenuBar menuBar;
+        JMenu menu;
+        JMenuItem menuItem;
+
+        //Create the menu bar.
+        menuBar = new JMenuBar();
+
+        //Build the first menu.
+        menu = new JMenu("Ayuda");
+        menuBar.add(menu);
+
+        //a group of JMenuItems
+        menuItem = new JMenuItem("Contenido de Ayuda", KeyEvent.VK_A);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
+
+        HelpSet hs = obtenFicAyuda();
+        HelpBroker hb = hs.createHelpBroker();
+        hb.enableHelpOnButton(menuItem, "menu", hs);
+
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+        return menuBar;
+    }
+
+    public Container createContentPane() {
+        //Create the content-pane-to-be.
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.setOpaque(true);
+
+        //Create a scrolled text area.
+        output = new JTextArea(5, 30);
+        output.setEditable(false);
+        scrollPane = new JScrollPane(output);
+
+        //Add the text area to the content pane.
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+
+        return contentPane;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
+    }
+
+    // Returns just the class name -- no package info.
+    protected String getClassName(Object o) {
+        String classString = o.getClass().getName();
+        int dotIndex = classString.lastIndexOf(".");
+        return classString.substring(dotIndex + 1);
+    }
+
+    /**
+     * Create the GUI and show it. For thread safety, this method should be
+     * invoked from the event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("MenuDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        LanzaAyuda demo = new LanzaAyuda();
+        frame.setJMenuBar(demo.createMenuBar());
+        frame.setContentPane(demo.createContentPane());
+
+        //Display the window.
+        frame.setSize(450, 260);
+        frame.setVisible(true);
+    }
+
+    public HelpSet obtenFicAyuda() {
+        try {
+            ClassLoader cl = LanzaAyuda.class.getClassLoader();
+//            File file = new File(LanzaAyuda.class.getResource("help/helpset.hs").getFile());
+            URL url = LanzaAyuda.class.getResource("help/helpset.hs");
+            // crea un objeto Helpset
+            HelpSet hs = new HelpSet(null, url);
+            return hs;
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Fichero HelpSet no encontrado");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+}
---
+++ src/lanzaayuda/help/helpset.hs
@@ -0,0 +1,18 @@
+<helpset version="2.0">
+ <maps>
+    <homeID>menu</homeID>
+    <mapref location="map.jhm" />
+ </maps>
+ <view xml:lang="en" mergetype="javax.help.UniteAppendMerge">
+    <name>TOC</name>
+    <label>Table Of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>TOc.xml</data>
+ </view>
+<view xml:lang="en" mergetype="javax.help.SortMerge">
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>index.xml</data>
+</view>
+</helpset>
